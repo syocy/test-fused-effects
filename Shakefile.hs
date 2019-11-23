8857d4daf1d710f60bba77ff4cb709d3875ad3a6
OSANAI Kazuyoshi <osmium.k@gmail.com>
2019-11-24 03:24:07 +0900

    fix workflow

--- Shakefile.hs
+++ Shakefile.hs
@@ -3,7 +3,6 @@
 build-depends: base ^>=4.13.0.0
              , shake
 -}
-{-# LANGUAGE LambdaCase #-}
 import Development.Shake
 import Development.Shake.Command
 import Development.Shake.FilePath
@@ -29,14 +28,6 @@ main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
     cmd "cabal v2-test"
 
   phony "scratch" $ do
-    doesDirectoryExist "/root/.ghcup" >>= \case
-      True -> cmd_ "cp -a /root/.ghcup $HOME"
-      False -> pure ()
-    doesDirectoryExist "/root/.cabal" >>= \case
-      True -> cmd_ "cp -a /root/.cabal $HOME"
-      False -> pure ()
-    cmd_ "cabal v2-configure -O1 --disable-documentation --write-ghc-environment-files=ghc8.4.4+"
-    -- cmd_ "luaotfload-tool -vvv --update"
     _ <- need ["test-fused-effects.pdf"]
     _ <- need ["clean"]
     pure ()
---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eux
+
+if [ -d /root/.cabal ] ; then
+    cp -a /root/.cabal $HOME
+fi
+
+if [ -d /root/.ghcup ] ; then
+    cp -a /root/.ghcup $HOME
+fi
+
+if [ $(whoami) = "root" ] ; then
+    luaotfload-tool -vvv --update
+fi
+
+cabal v2-configure -O1 --disable-documentation --write-ghc-environment-files=ghc8.4.4+
+
+./Shakefile.hs scratch
