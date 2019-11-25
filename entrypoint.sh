800abaeb48a94e8c3ff7c8b9646a0e6373283855
OSANAI Kazuyoshi <osmium.k@gmail.com>
2019-11-25 23:18:14 +0900

    fix workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,9 @@ if [ $(whoami) = "root" ] ; then
     luaotfload-tool -vvv --update
 fi
 
-cabal v2-configure -O1 --disable-documentation --write-ghc-environment-files=ghc8.4.4+
+if [ ! -e "cabal.project.local" ] ; then
+    cabal v2-configure -O1 --disable-documentation --write-ghc-environment-files=ghc8.4.4+
+fi
 
 ./Shakefile.hs
 ./Shakefile.hs clean
